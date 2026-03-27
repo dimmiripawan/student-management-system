7a5b260fa5feb46099765214edbf431997740f25
Your Name <dimmiripawan@gmail.com>
2026-03-27 17:11:49 +0530

    Student Services

--- studentapp/src/main/java/com/example/studentapp/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.studentapp.repository;
-
-import com.example.studentapp.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
